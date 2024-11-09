93ef81c611047b5033841a899345de5be78266f0
KietCSE <ptkiet170104@gmail.com>
2024-11-09 08:53:22 +0700

    add: router-react-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
-
+import TK from './TK-main.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TK />
   </StrictMode>,
 )
